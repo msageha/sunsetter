130f6e6ea73f743c53efbea2258060e526e1b24a
fono09 <fono@fono.jp>
2017-10-29 15:55:25 +0900

    Last commit

--- ddl/follows.sql
+++ ddl/follows.sql
@@ -3,5 +3,7 @@ create table follows
     id bigint unsigned primary key auto_increment,
     src_user_id int unsigned not null,
     dst_user_id int unsigned not null,
-    created_at datetime not null default current_timestamp
+    created_at datetime not null default current_timestamp,
+    INDEX idx_src_user_id(src_user_id),
+    INDEX idx_dst_user_id(dst_user_id)
   );
--- ddl/post_images.sql
+++ ddl/post_images.sql
@@ -1,5 +1,6 @@
 create table post_images (
   id bigint unsigned primary key auto_increment,
   post_id bigint unsigned not null,
-  image mediumblob not null
+  image mediumblob not null,
+  INDEX idx_post_id(post_id)
 );
