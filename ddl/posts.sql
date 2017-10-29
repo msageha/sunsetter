3ed4ad37819c18ffcb0ca36f01ab8284853a0e44
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-10-29 10:01:27 +0900

    fix columns

--- ddl/posts.sql
+++ ddl/posts.sql
@@ -2,5 +2,5 @@ create table posts (
   id bigint unsigned primary key auto_increment,
   message varchar(200) not null,
   user_id int unsigned not null,
-  created_at datetime default current_timestamp
+  created_at datetime not null default current_timestamp
 );
